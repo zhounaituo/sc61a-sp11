54755afe91a9c59e9b71d57e1349a07dd327463f
zhounaituo <zhounaituo@gmail.com>
2024-01-28 10:18:22 +0800

    阅读至 32 页

---
+++ Chapter1/practice/cube-root.scm
@@ -0,0 +1,12 @@
+(define (cube-iter guess x)
+  (if (good-enough? guess x)
+      guess
+      (cube-iter (improve guess x) x)))
+(define (improve guess x)
+  (/ (+ (/ x (square guess)) (* 2 guess)) 3))
+(define (square x)
+  (* x x))
+(define (good-enough? guess x)
+  (< (abs (- guess (/ x guess guess))) 0.001))
+(define (cube x)
+  (cube-iter 1.0 x))
