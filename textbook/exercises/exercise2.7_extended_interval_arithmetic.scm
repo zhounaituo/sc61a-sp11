f8bd9484776e2753692a3b1fd9104428d156c0ad
zhounaituo <zhounaituo@gmail.com>
2024-12-15 17:17:04 +0800

    2024-12-15 17:17:01

--- textbook/exercises/exercise2.7_extended_interval_arithmetic.scm
+++ textbook/exercises/exercise2.7_extended_interval_arithmetic.scm
@@ -216,3 +216,20 @@
 ;   = [(1-(p1+p2)+p1p2)c1c2,(1+(p1+p2)+p1p2)c1c2]
 ; I = [c1c2±(p1p2)c1c2]
 ; p = (p1p2)
+
+; Exercise 2.14. Demonstrate that Lem is right. Investigate the behavior 
+; of the system on a variety of arithmetic expressions. Make some 
+; intervals A and B, and use them in computing the expressions A/A and A/
+; B. You will get the most insight by using intervals whose width is a 
+; small percentage of the center value. Examine the results of the 
+; computation in center-percent form (see exercise 2.12).
+
+(define (par1 r1 r2)
+    (div-interval (mul-interval r1 r2)
+                  (add-interval r1 r2)))
+
+(define (par2 r1 r2)
+    (let ((one (make-interval 1 1)))
+        (div-interval
+         one (add-interval (div-interval one r1)
+                           (div-interval one r2)))))
