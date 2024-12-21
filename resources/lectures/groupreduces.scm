bf3d01a2cc6cdd162164cc9a88b20aae53ca72da
ZhouNaituo <48860602+zhounaituo@users.noreply.github.com>
2024-12-21 14:59:22 +0800

    2024-12-21 14:59:21

--- resources/lectures/groupreduces.scm
+++ resources/lectures/groupreduces.scm
@@ -1,127 +1,152 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-     <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-     <meta charset="utf-8" />
-     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-     <meta name="verify-v1" content="JGaSHUCkIAbgSRQ5nzWiKM4P8Vl1dtfBeLFXc8w2SO4=" />
-     <meta name="google-site-verification" content="bzRPpkNkzyctlxDyqPXvqaCom3-9L7qM7XlU4W7ZyG0" />
-     <meta name="description" content="Electrical Engineering and Computer Sciences is the largest department at the University of California, Berkeley.  EECS spans all of information science and technology and has applications in a broad range of fields, from medicine to the social sciences." />
-     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+; Pseudo-mapreduce on one processor to illustrate the concept early.
 
-     <link href="/Includes/style.css" rel="stylesheet" type="text/css">
-<style type="text/css" media="all">@import "/Includes/grandfather.css";</style>
-<link rel="stylesheet" type="text/css" media="print" href="/Includes/print.css" />
+(define make-kv-pair cons)
+(define kv-key car)
+(define kv-value cdr)
 
-<title>Resource not found | EECS UC Berkeley</title>
+(define (groupreduce fn base buckets)
+  (map (lambda (subset)
+	 (make-kv-pair (kv-key (car subset))
+		       (accumulate fn base (map kv-value subset))))
+       buckets))
 
-</head>
-<body>
+(define (sort-into-buckets alist)
+  (group (sort-by-key alist)))
 
-<!-- Google Tag Manager -->
-<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-WZMWWC"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-WZMWWC');</script>
-<!-- End Google Tag Manager -->
+;;;;;;;;;;
 
-<ul id="skip">
-	<li><a href="#search-anchor">Skip to Search</a></li>
-	<li><a href="#nav-anchor">Skip to Nav</a></li>
-	<li><a href="#main-anchor">Skip to Main Content</a></li>
-	<li><a href="#footer-anchor">Skip to Footer</a></li>
-</ul>
+(define (group alist)
+  (define (help in complete current)
+    (cond ((null? in)
+	   (reverse (cons (reverse current) complete)))
+	  ((null? current)
+	   (help (cdr in) complete (cons (car in) current)))
+	  ((equal? (kv-key (car in)) (kv-key (car current)))
+	   (help (cdr in) complete (cons (car in) current)))
+	  (else
+	   (help in (cons (reverse current) complete) '()))))
+  (if (null? alist)
+      '()	; Return (), not (()), if no data at all.
+      (help alist '() '())))
 
-<a name="top"></a>
+(define (sort-by-key alist)
+  (sort alist super-before?))
 
-  <div id="header" role="banner">
-<!--#include virtual="/Includes/TopBanner.shtml"-->
-<!--#include virtual="/Includes/TopNav.shtml"-->
-</div> <!--end header -->
+(define (super-before? a b)
+  (cond ((and (number? (kv-key a)) (number? (kv-key b)))
+	 (<= (kv-key a) (kv-key b)))
+	((and (word? (kv-key a)) (word? (kv-key b)))
+	 (not (before? (kv-key b) (kv-key a))))
+	((and (pair? (kv-key a)) (pair? (kv-key b)))
+	 (and (not (before? (car (kv-key b)) (car (kv-key a))))
+	      (not (super-before? (cdr (kv-key b)) (cdr (kv-key a))))))
+	(else #t)))	; punt if not comparable
 
-<a name="main-anchor" class="accessibility" tabindex="0">main</a>
-  <div id="content" role="main">
+;;;;;;;;;;
 
+(define filename car)
+(define lines cdr)
 
+(define (file->linelist file)
+  (map (lambda (line) (make-kv-pair (filename file) line))
+       (lines file)))
 
-<script type="text/javascript">
-<!--
+(define (match? pattern sent)
+  (cond ((null? pattern) (null? sent))
+	((null? sent) (and (eq? (car pattern) '*)
+			   (match? (cdr pattern) sent)))
+	((eq? (car pattern) '*) (or (match? (cdr pattern) sent)
+				    (match? pattern (cdr sent))))
+	((eq? (car pattern) (car sent)) (match? (cdr pattern) (cdr sent)))
+	(else #f)))
 
-function validate() {
-  var val = document.contact.message.value
-  if ( val == '' ) {
-     alert ( "Please include a Message" )
-     return false
-  }
-}
+; Some sample data
 
-//-->
-</script>
-<!-- <style type="text/css" media="all">@import
-"/ErrorDocuments/webteamErrorPage.css";</style> -->
+(define mt1 '((cs61a-xc . 27) (cs61a-ya . 40) (cs61a-xw . 35)
+	      (cs61a-xd . 38) (cs61a-yb . 29) (cs61a-xf . 32)))
+(define mt2 '((cs61a-yc . 32) (cs61a-xc . 25) (cs61a-xb . 40)
+	      (cs61a-xw . 27) (cs61a-yb . 30) (cs61a-ya . 40)))
+(define mt3 '((cs61a-xb . 32) (cs61a-xk . 34) (cs61a-yb . 30)
+	      (cs61a-ya . 40) (cs61a-xc . 28) (cs61a-xf . 33)))
 
-<h3>The server has encountered a problem because the resource was not found.</h3>
+(define file1 '((please please me) (i saw her standing there) (misery)
+		(anna go to him) (chains) (boys) (ask me why)
+		(please please me) (love me do) (ps i love you)
+		(baby its you) (do you want to know a secret)))
+(define file2 '((with the beatles) (it wont be long) (all ive got to do)
+		(all my loving) (dont bother me) (little child)
+		(till there was you) (roll over beethoven) (hold me tight)
+		(you really got a hold on me) (i wanna be your man)
+		(not a second time)))
+(define file3 '((a hard days night) (a hard days night) 
+		(i should have known better) (if i fell)
+		(im happy just to dance with you) (and i love her)
+		(tell me why) (cant buy me love) (any time at all)
+		(ill cry instead) (things we said today) (when i get home)
+		(you cant do that) (ill be back)))
 
-<dl>
-<dt>Your request was :</dt>
-<dd>https://people.eecs.berkeley.edu/~bh/61a-pages/Lectures/groupreduces.scm</dd>
-</dl>
+; demos
 
-<h3>What are you looking for?</h3>
-<ul>
-    <li><A HREF="http://www.eecs.berkeley.edu/Gradadm/">
-Graduate Applications &amp; Information</A></li>
-    <li><a href="http://www.eecs.berkeley.edu/Prospective/ugrad.shtml">
-Undergraduate EECS and CS Course Requirements, Transfer &amp; Admissions Information</a></li>
-	      
-    <li><A HREF="http://www.eecs.berkeley.edu/Announcements/">
-How to post job, conference and other outside announcements to the
-EECS Department</a></li>
-   <li><a href="http://iris.eecs.berkeley.edu/helpdesk/">
-EECS Departmental Computer or Network Support</a></li>
-   <li><a href="http://www.eecs.berkeley.edu/department/people.shtml">
-People</a></li>
-</ul>
+; (sort-into-buckets (append mt1 mt2 mt3))
 
-<h3>Contact Us</h3>
-if (bh@cs != '') {
-   bh@cs = str_replace("@", " AT ", bh@cs);
-   mailto:webteam%20AT%20eecs.berkeley.edu?subject=Not Found URL: https%3A%2F%2Fpeople.eecs.berkeley.edu%2F~bh%2F61a-pages%2FLectures%2Fgroupreduces.scm&amp;body=Link%3A%20https%3A%2F%2Fpeople.eecs.berkeley.edu%2F~bh%2F61a-pages%2FLectures%2Fgroupreduces.scm%0AReferrer%3A%20="mailto:bh@cs.berkeley.edu?subject=Not Found URL: https%3A%2F%2Fpeople.eecs.berkeley.edu%2F~bh%2F61a-pages%2FLectures%2Fgroupreduces.scm&body=Link%3A%20https%3A%2F%2Fpeople.eecs.berkeley.edu%2F~bh%2F61a-pages%2FLectures%2Fgroupreduces.scm%0AReferrer%3A%20";
-   mailto:webteam%20AT%20eecs.berkeley.edu?subject=Not Found URL: https%3A%2F%2Fpeople.eecs.berkeley.edu%2F~bh%2F61a-pages%2FLectures%2Fgroupreduces.scm&amp;body=Link%3A%20https%3A%2F%2Fpeople.eecs.berkeley.edu%2F~bh%2F61a-pages%2FLectures%2Fgroupreduces.scm%0AReferrer%3A%20=htmlentities(mailto:webteam%20AT%20eecs.berkeley.edu?subject=Not Found URL: https%3A%2F%2Fpeople.eecs.berkeley.edu%2F~bh%2F61a-pages%2FLectures%2Fgroupreduces.scm&amp;body=Link%3A%20https%3A%2F%2Fpeople.eecs.berkeley.edu%2F~bh%2F61a-pages%2FLectures%2Fgroupreduces.scm%0AReferrer%3A%20);
+; (groupreduce + 0 (sort-into-buckets (append mt1 mt2 mt3)))
 
-   print("<p>To contact the owner of the specific page you were
-   looking for send an email to:</p>");
-   print("<code style=\"margin: 20px; padding: 0 20px ;\"><a href=\"mailto:webteam%20AT%20eecs.berkeley.edu?subject=Not Found URL: https%3A%2F%2Fpeople.eecs.berkeley.edu%2F~bh%2F61a-pages%2FLectures%2Fgroupreduces.scm&amp;body=Link%3A%20https%3A%2F%2Fpeople.eecs.berkeley.edu%2F~bh%2F61a-pages%2FLectures%2Fgroupreduces.scm%0AReferrer%3A%20\">bh@cs</a></code>");
-   }
+'(groupreduce (lambda (new old) (+ 1 old)) 0
+	      (sort-into-buckets (append mt1 mt2 mt3)))
 
+; (map (lambda (wd) (list (make-kv-pair wd 1))) '(cry baby cry))
 
-<p>
-To report problems or submit questions and comments related to the
-site, please send an email to the following email address:</p>
+(define (wordcounts1 sent)
+  (groupreduce + 0 (sort-into-buckets (map (lambda (wd) (make-kv-pair wd 1))
+					   sent))))
 
-<code style="margin: 20px; padding: 0 20px ;">
-<a href="mailto:webteam%20AT%20eecs.berkeley.edu?subject=Not Found URL: https%3A%2F%2Fpeople.eecs.berkeley.edu%2F~bh%2F61a-pages%2FLectures%2Fgroupreduces.scm&amp;body=Link%3A%20https%3A%2F%2Fpeople.eecs.berkeley.edu%2F~bh%2F61a-pages%2FLectures%2Fgroupreduces.scm%0AReferrer%3A%20">webteam AT EECS.Berkeley.EDU</a></code>
-</p>
-<!-- debug info
-<dl>
-<dt><em>Debugging Info:</em></dt>
-<dt>This resource is likely managed by :</dt>
-<dd>user: bh</dd>
-<dd>ldap count: (1)</dd>
-<dd>Code: (404)</dd>
-<dd>Local Email: bh@cs</dd>
-<dd>Name: Brian Harvey</dd>
-</dl>
--->
+; (wordcounts1 '(cry baby cry))
 
+; (file->linelist file1)
 
-    </div> <!-- end content div -->
+(define (wordcounts files)
+  (groupreduce + 0 (sort-into-buckets
+		    (flatmap (lambda (kv-pair)
+			       (map (lambda (wd) (make-kv-pair wd 1))
+				    (kv-value kv-pair)))
+			     files))))
 
-<!--#include virtual="/Includes/webteamFooter.shtml"-->
-  </body>
+'(wordcounts (append (file->linelist file1)
+		     (file->linelist file2)
+		     (file->linelist file3)))
 
-</html>
+(define (mostfreq files)
+  (accumulate (lambda (new old)
+		(cond ((> (kv-value new) (kv-value (car old)))
+		       (list new))
+		      ((= (kv-value new) (kv-value (car old)))
+		       (cons new old))	; In case of tie, remember both.
+		      (else old)))
+	      (list (make-kv-pair 'foo 0))	; Starting value.
+	      (groupreduce + 0 (sort-into-buckets
+				(flatmap (lambda (kv-pair)
+					   (map (lambda (wd)
+						  (make-kv-pair wd 1))
+						(kv-value kv-pair)))
+					 files)))))
 
+'(mostfreq (append (file->linelist file1)
+		   (file->linelist file2)
+		   (file->linelist file3)))
+
+; (match? '(* i * her *) '(i saw her standing there))
+; (match? '(* i * her *) '(and i love her))
+; (match? '(* i * her *) '(ps i love you))
+
+(define (grep pattern files)
+  (groupreduce cons '()
+	       (sort-into-buckets
+		(flatmap (lambda (kv-pair)
+			   (if (match? pattern (kv-value kv-pair))
+			       (list kv-pair)
+			       '()))
+			 files))))
+
+'(grep '(* i * her *) (append (file->linelist file1)
+			      (file->linelist file2)
+			      (file->linelist file3)))
